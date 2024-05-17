67d6d95b55579119c621db7a6c37708e620a4262
rui2832 <3122008@nagoya-bunri.ac.jp>
2024-05-17 16:32:36 +0900

    Initial Commit

---
+++ ARtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARtest
+//
+//  Created by ichinose-PC on 2024/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
