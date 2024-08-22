d4892fb895b93bfe3285405176c66303a5b44044
nose3122008 <3122008@nagoya-bunri.ac.jp>
2024-08-22 21:31:16 +0900

    initial commit

--- ARtest/ViewController.swift
+++ ARtest/ViewController.swift
@@ -6,14 +6,73 @@
 //
 
 import UIKit
+import SceneKit
+import ARKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        
+        // first see if there is a folder called "ARImages" Resource Group in our Assets Folder
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "ARResources", bundle: Bundle.main) {
+            
+            // if there is, set the images to track
+            configuration.trackingImages = trackedImages
+            // at any point in time, only 1 image will be tracked
+            configuration.maximumNumberOfTrackedImages = 1
+        }
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        // if the anchor is not of type ARImageAnchor (which means image is not detected), just return
+        guard let imageAnchor = anchor as? ARImageAnchor else {return}
+        //find our video file
+        let videoNode = SKVideoNode(fileNamed: "knmc.mp4")
+        videoNode.play()
+        // set the size (just a rough one will do)
+        let videoScene = SKScene(size: CGSize(width: 1720, height: 1080))
+        // center our video to the size of our video scene
+        videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+        // invert our video so it does not look upside down
+        videoNode.yScale = -1.0
+        // add the video to our scene
+        videoScene.addChild(videoNode)
+        // create a plan that has the same real world height and width as our detected image
+        let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+        // set the first materials content to be our video scene
+        plane.firstMaterial?.diffuse.contents = videoScene
+        // create a node out of the plane
+        let planeNode = SCNNode(geometry: plane)
+        // since the created node will be vertical, rotate it along the x axis to have it be horizontal or parallel to our detected image
+        planeNode.eulerAngles.x = -Float.pi / 2
+        // finally add the plane node (which contains the video node) to the added node
+        node.addChildNode(planeNode)
+    }
 }
-
